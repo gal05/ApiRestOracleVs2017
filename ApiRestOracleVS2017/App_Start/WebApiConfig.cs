60a2545e7679dace80facadc9c57a94471d41d49
jose <jose.guerra.g@tecsup.edu.pe>
2017-12-21 22:09:00 -0500

    formato Json

--- ApiRestOracleVS2017/App_Start/WebApiConfig.cs
+++ ApiRestOracleVS2017/App_Start/WebApiConfig.cs
@@ -25,6 +25,10 @@ namespace ApiRestOracleVS2017
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
 }
